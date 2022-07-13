f907cc0137f36d73fc54726b3ccbb4ed83fe5dbd
girls-dream <2330964358@qq.com>
2022-07-13 09:49:16 +0800

    清理项目完成

--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 加载全局样式
+
+import './styles/index.css'
+
+import 'amfe-flexible'
+
 Vue.config.productionTip = false
 
 new Vue({
